0b641af292f124985008d70d9e2d44c3929c503e
Matthew Duffy <mattduffy@gmail.com>
2023-01-15 15:42:23 -0800

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+/**
+ * @module @mattduffy/koa-cors
+ * @author Matthew Duffy <mattduffy@gmail.com>
+ * @file src/index.js The entry point for the CORS middleware for Koa app.
+ */
+
---
+++ test/koa-cors.test.js
@@ -0,0 +1,6 @@
+/**
+ * @module @mattduffy/koa-cors
+ * @author Matthew Duffy <mattduffy@gmail.com>
+ * @file test/koa-cors.test.js The spec file for testing koa-cors middleware.
+ */
+
